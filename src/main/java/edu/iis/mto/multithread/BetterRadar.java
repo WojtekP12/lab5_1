f8e2dc6a9db0267c34a8bfdc73edb3a4d3d0ab69
Wojciech Pełka <nero1993@onet.pl>
2016-06-15 20:21:53 +0200

    BetterRadar class added

---
+++ src/main/java/edu/iis/mto/multithread/BetterRadar.java
@@ -0,0 +1,35 @@
+package edu.iis.mto.multithread;
+
+import java.util.concurrent.Executor;
+
+public class BetterRadar 
+{
+	private PatriotBattery battery;
+	private Executor executor;
+	
+	public BetterRadar(PatriotBattery missle, Executor executor) 
+	{
+		this.battery = missle;
+		this.executor = executor;
+	}
+
+	public void notice(Scud enemyMissle) 
+	{
+		launchPatriot();
+	}
+	
+	private void launchPatriot() 
+	{
+		executor.execute(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				for (int i = 0; i < 10; i++) 
+				{
+					battery.launchPatriot();
+				}
+			}
+		});
+	}
+}
